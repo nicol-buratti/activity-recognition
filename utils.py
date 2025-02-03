38474c3c6f8fa49bec0cd1b3d569f5ee2c191664
Diezz01 <diegopennesi01@gmail.com>
2025-02-03 12:10:51 +0100

    added utils

---
+++ utils.py
@@ -0,0 +1,22 @@
+from decord import VideoReader, cpu
+import numpy as np
+
+
+NUM_FRAMES = 100
+MAX_LENGTH = 30_000
+
+def read_video_decord(video_path, num_frames=NUM_FRAMES):
+    '''
+    Decode the video with Decord decoder.
+
+    Args:
+        video_path (str): Path to the video file.
+        num_frames (int): Number of frames to sample uniformly. Defaults to NUM_FRAMES
+
+    Returns:
+        np.ndarray: np array of decoded frames of shape (num_frames, height, width, 3).
+    '''
+    vr = VideoReader(uri=video_path, ctx=cpu(0)) # you need to install from source to use gpu ctx
+    indices = np.arange(0, len(vr), len(vr) / num_frames).astype(int)
+    frames = vr.get_batch(indices).asnumpy()
+    return frames
