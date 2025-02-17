276d505b8bb8d8f3d7b6139264b91cb4914198e1
Diezz01 <diegopennesi01@gmail.com>
2025-02-17 11:45:44 +0100

    agent intergation in frontend

--- frontend.py
+++ frontend.py
@@ -1,37 +1,37 @@
+from concurrent.futures import ProcessPoolExecutor
 import os
 from pathlib import Path
 import streamlit as st
 import torch
 from langchain.agents import initialize_agent
 from langchain.agents.agent import AgentType
-
+from langchain_core.messages import HumanMessage
+from langgraph.checkpoint.memory import MemorySaver
+from langgraph.prebuilt import create_react_agent
+from langchain.agents import AgentExecutor
+from ultralytics import YOLO
+from langchain import hub
+from tools import ObjectDetectionTool, VideoActivityRecognitionTool
 from langchain.memory import ConversationBufferMemory
 from loguru import logger
 from ultralytics import YOLO
 
 from tools import ObjectDetectionTool, VideoActivityRecognitionTool, llm
 
+YOLO_PATH = "runs/classify/custom_yolo7/weights/best.pt" # TODO add env variable
 
 class App:
     def __init__(self, device) -> None:
-        # if "agent" not in st.session_state:
-        self._agent = initialize_agent(
-            agent=AgentType.STRUCTURED_CHAT_ZERO_SHOT_REACT_DESCRIPTION,
-            tools=[
-                ObjectDetectionTool().setup(
-                    YOLO(
-                        "C:/Users/Informatica_UNICAM/Desktop/activity-recognition/"
-                        "runs/detect/custom_yolo7/weights/best.pt"
-                    )
-                ),
-                # VideoActivityRecognitionTool().setup(llm),
-            ],
-            llm=llm,
-            memory=ConversationBufferMemory(return_messages=True),
-            verbose=True,
-            max_iterations=3,
-        )
-        st.session_state["agent"] = self._agent
+        self.llm = llm
+        memory = MemorySaver()
+
+        if "video_agent" not in st.session_state:
+            video_tools = [VideoActivityRecognitionTool().setup()]
+            st.session_state.video_agent = create_react_agent(llm, video_tools, checkpointer=memory)
+
+        if "obj_agent" not in st.session_state:
+            obj_tools = [ObjectDetectionTool().setup(YOLO(Path(YOLO_PATH).absolute(),verbose = False))]
+            st.session_state.obj_agent = create_react_agent(llm, obj_tools,  checkpointer=memory)
 
     def _upload_image(self) -> None:
         uploaded_image = st.file_uploader("Upload an image")
@@ -44,14 +44,20 @@ class App:
             st.sidebar.image(temp_file_path, width=200)
             self._process_image(temp_file_path)
 
-    def _process_image(self, image_path: str) -> None:
+    def _process_file(self, prompt, file_path: str) -> None:
         try:
-            result = self._agent(
-                f"Describe the image: {image_path} and detect objects with the description."
-            )
-            logger.debug(result)
+            config = {"configurable": {"thread_id": "abc123"}}
+            with ProcessPoolExecutor() as ex: #TODO parallel execution
+                video_description = st.session_state.video_agent.invoke({"messages":[HumanMessage(content=prompt + f". Given the following video:{file_path}.")]}, config)
+                object_response = st.session_state.obj_agent.invoke({"messages":[HumanMessage(content=prompt + f". Given the following video:{file_path}.")]}, config)
+            
+            logger.debug(f"{video_description=}")
+            logger.debug(f"{object_response=}")
+
+            return video_description["messages"][-1].content, object_response["messages"][-1].content
         except Exception as e:
             logger.error(e)
+
 
     def _copy_file(self, uploaded_image):
         tmp_dir = "tmp/"
@@ -64,8 +70,7 @@ class App:
         st.title("Chat with LLava")
 
         if "chat" not in st.session_state:
-            client = llm
-            st.session_state.chat = client
+            st.session_state.chat = self.llm
 
         if "messages" not in st.session_state:
             st.session_state.messages = []
@@ -80,30 +85,38 @@ class App:
         
         # display the chat messages
         for message in st.session_state.messages:
-            with st.chat_message(message["role"]):
-                for item in message["content"]:
-                    if item["type"] == "text":
-                        st.markdown(item["text"])
-                    if item["type"] == "image":
-                        st.image(str(item["path"]))
-                    if item["type"] == "video":
-                        st.video(str(item["path"]))
+            with st.chat_message(message.type):
+                st.markdown(message.content.split(". Answer given")[0])
+                if "file_path" not in message.additional_kwargs:
+                    continue
+                
+                # image
+                if message.additional_kwargs["file_path"].suffix[1:] in ["jpg", "jpeg", "png"]:
+                    st.image(str(message.additional_kwargs["file_path"]))
+                
+                # video
+                if message.additional_kwargs["file_path"].suffix[1:] in ["mp4", "mkv"]:
+                    st.video(str(message.additional_kwargs["file_path"]))
 
         if submit_button:
-            content = []
-            message = {"role": "user", "content": content}
-            if prompt:
-                content.append({"type": "text", "text": prompt})
+            message = HumanMessage(content=prompt)
+
             if uploaded_file is not None:
                 self._copy_file(uploaded_file)
                 tmp_path = Path(f"tmp/{uploaded_file.name}")
-                if "image" in uploaded_file.type:
-                    content.append({"type": "image", "path": tmp_path})
-                if "video" in uploaded_file.type:
-                    content.append({"type": "video", "path": tmp_path})
+                
+                video_description, obj_detection = self._process_file(message.content, tmp_path)
+                message.content += ". Answer given these informations about the file, don't write the file path:\n"
+                if video_description:
+                    message.content += f"{video_description=}\n"
+                if obj_detection:
+                    message.content += f"{obj_detection=}\n"
+                
+                # .additional_kwargs.update({"video_description":video, "object_description":obj})
+                message.additional_kwargs.update({"file_path":tmp_path})
 
             # Append user's message to session state
-            st.session_state.messages.append(message.copy())  # TODO check if is correct
+            st.session_state.messages.append(message)
             with st.chat_message("user"):
                 if prompt:
                     st.markdown(prompt)
@@ -115,19 +128,12 @@ class App:
 
             # Get response from the LLM
             with st.chat_message("assistant"):
-                response = st.session_state.chat.invoke(
-                    [
-                        {"role": m["role"], "content": m["content"]}
-                        for m in st.session_state.messages
-                    ]
-                )
-                st.markdown(response)
-                st.session_state.messages.append(
-                    {
-                        "role": "assistant",
-                        "content": [{"type": "text", "text": response}],
-                    }
-                )
+                print(st.session_state.messages)
+                response = st.session_state.chat.invoke(st.session_state.messages)
+                response.pretty_print()
+                st.markdown(response.content)
+                st.session_state.messages.append(response)
+            print(f"{st.session_state.messages=}")
 
 
 if __name__ == "__main__":
--- tools.py
+++ tools.py
@@ -1,4 +1,5 @@
 import os
+from pathlib import Path
 import sys
 import time
 from typing import Optional
@@ -14,8 +15,9 @@ from transformers import (
     BitsAndBytesConfig,
 )
 import torch
+from langchain.chat_models import init_chat_model
 
-from utils import MAX_LENGTH, read_video_decord
+from training_notebooks.llava.utils import MAX_LENGTH, read_video_decord
 from dotenv import load_dotenv
 load_dotenv()
 
@@ -23,81 +25,43 @@ REPO_ID = os.getenv('REPO_ID')
 
 device = "cuda" if torch.cuda.is_available() else "cpu"
 
-quantization_config = BitsAndBytesConfig(
-    load_in_4bit=True, bnb_4bit_compute_dtype=torch.float16
-)
-
-model = LlavaNextVideoForConditionalGeneration.from_pretrained(
-    REPO_ID,
-    quantization_config=quantization_config,
-    device_map=device,
-).to(device)
-
-processor = LlavaNextVideoProcessor.from_pretrained(REPO_ID)
-
-def map_conversation_to_llava_format(conversation, videos, images):
-    conversation_copy = []
-    for conv in conversation:
-        conv_line = {"role": conv["role"], "content": []}
-        for item in conv["content"]:
-            item_copy = item.copy()
-            if item["type"] == "video":
-                path = item_copy.pop("path")
-                clip = read_video_decord(str(path))
-                videos.append(clip)
-            if item["type"] == "image":
-                raw_image = Image.open(item_copy.pop("path"))
-                images.append(raw_image)
-
-            conv_line["content"].append(item_copy)
-        conversation_copy.append(conv_line)
-    return conversation_copy
-
-def run_llava(conversation):
-    videos, images  = [], []
-    conversation_copy = map_conversation_to_llava_format(conversation, videos, images)
-    prompt = processor.apply_chat_template(
-        conversation_copy, add_generation_prompt=True
-    )
-
-    # processor format
-    if not videos:
-        videos = None
-    if not images:
-        images = None
-
-    inputs_video = processor(
-                text=prompt, videos=videos, images=images, padding=True, return_tensors="pt"
-            ).to(model.device)
-
-    output = model.generate(**inputs_video, do_sample=False, max_new_tokens=50_000)
-    generated_text = processor.decode(output[0][2:], skip_special_tokens=True)
-    _, _, generated_text = generated_text.rpartition("ASSISTANT:")
-    return generated_text
-
-
-# Wrap it as a Runnable
-llm = RunnableLambda(run_llava)
-
+model_id = "llama3.2:1b" # TODO add to .env
+llm = init_chat_model(model_id, model_provider="ollama", temperature=0.1)
 
 logger.remove()
-logger.add(sys.stderr, level="INFO")
-
+logger.add(sys.stderr, level="DEBUG")
 
 class VideoActivityRecognitionTool(BaseTool):
-    name: str = "Video activity detection tool"
-    description: str = "Classifies video content to detect and recognize activities performed by robotic tools, enabling accurate action identification for automated analysis."
+    name: str = "Video Activity Description Tool"
+    description: str = (
+        "Generates a descriptive summary of the activities or scenes in a video, only if provided. "
+        "Provide the video path to receive an activity description based on its content."
+    )
 
     llm: Optional[LlavaNextVideoForConditionalGeneration] = None
+    processor:Optional[LlavaNextVideoProcessor] = None
 
-    def setup(
-        self, llm: LlavaNextVideoForConditionalGeneration
-    ) -> "VideoActivityRecognitionTool":
-        self.llm = llm
+    def setup(self) -> "VideoActivityRecognitionTool":
+        quantization_config = BitsAndBytesConfig(
+            load_in_4bit=True, bnb_4bit_compute_dtype=torch.float16
+        )
+
+        model = LlavaNextVideoForConditionalGeneration.from_pretrained(
+            REPO_ID,
+            quantization_config=quantization_config,
+            device_map=device,
+        ).to(device)
+
+        model_processor = LlavaNextVideoProcessor.from_pretrained(REPO_ID)
+        self.llm = model
+        self.processor = model_processor
         return self
 
     def _run(self, video_path: str) -> str:
-        logger.debug(f"Video path: {video_path}")
+        if not (Path(video_path).exists() and Path(video_path).is_file()):
+            return "no video"
+
+        logger.debug(f"Video path video recognition: {video_path}")
         video = read_video_decord(video_path)
         conversation = [
             {
@@ -105,31 +69,24 @@ class VideoActivityRecognitionTool(BaseTool):
                 "content": [
                     {
                         "type": "text",
-                        "text": "You are working in an industrial setting where robotic arms perform various activities. Your task is to analyze videos of these robotic arms in action and accurately classify the specific activity being performed in each video. Answer only with the activity detected.",
+                        "text": "Describe this video",
                     },
                     {"type": "video"},
                 ],
             },
         ]
-
-        prompt = processor.apply_chat_template(conversation, add_generation_prompt=True)
-
-        batch = processor(
-            text=prompt,
-            videos=video,
-            truncation=True,
-            max_length=MAX_LENGTH,
-            return_tensors="pt",
-        ).to(device)
-
-        video = video.to(llm.device)
-
-        out = llm.generate(
-            **batch, pixel_values_videos=video, max_length=MAX_LENGTH, do_sample=True
+        prompt = self.processor.apply_chat_template(
+            conversation, add_generation_prompt=True
         )
-        generated_text = processor.batch_decode(out, skip_special_tokens=True)
 
-        logger.debug(f"Generated prompt: {generated_text}")
+        inputs_video = self.processor(
+                    text=prompt, videos=video, padding=True, return_tensors="pt"
+                ).to(self.llm.device)
+
+        output = self.llm.generate(**inputs_video, do_sample=False, max_new_tokens=50_000)
+        generated_text = self.processor.decode(output[0], skip_special_tokens=True)
+        _, _, generated_text = generated_text.rpartition("ASSISTANT: ")
+        logger.debug(f"{generated_text=}")
         return generated_text
 
     def _arun(self, query: str):
@@ -137,9 +94,9 @@ class VideoActivityRecognitionTool(BaseTool):
 
 
 class ObjectDetectionTool(BaseTool):
-    name: str = "Object detection on image tool"
+    name: str = "Video Object Detection Tool"
     description: str = (
-        "Perform object detection on an image (read an image path) with a text prompt."
+        "Performs object detection on a video file. Given the video path, it identifies and returns objects detected in the video frames."
     )
 
     model: Optional[YOLO] = None
@@ -148,22 +105,13 @@ class ObjectDetectionTool(BaseTool):
         self.model = _model
         return self
 
-    def _run(self, image_path, prompt: str) -> str:
-        logger.debug(f"Image path: {image_path}, prompt: {prompt}")
+    def _run(self, video_path) -> str:
+        if not (Path(video_path).exists() and Path(video_path).is_file()):
+            return "no video"
+        logger.debug(f"Video path object detection: {video_path}")
 
-        if not self.model:
-            raise ValueError(
-                "The model has not been set up. Please call `setup` first."
-            )
-
-        result = self.model(image_path["title"])
-        output_dir = "output/"
-        os.makedirs(output_dir, exist_ok=True)
-        now_time = time.strftime("%Y-%m-%d-%H-%M-%S", time.localtime())
-        output_img_path = os.path.join(output_dir, f"{now_time}_obj_detection.png")
-        result[0].save(filename=output_img_path)
-
-        return Image.open(output_img_path)
+        results =  self.model(str(Path(video_path).absolute()))
+        return [res.names[res.probs.top1] for res in results]
 
     def _arun(self, query: str):
         raise NotImplementedError
