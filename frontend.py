1e2115bfce41ac8a10319530e8ea3beb2501e09b
Diezz01 <diegopennesi01@gmail.com>
2025-02-24 17:52:19 +0100

    single agent with multiple tool, parallel tracking

--- frontend.py
+++ frontend.py
@@ -14,76 +14,54 @@ from dotenv import load_dotenv
 from  moviepy import VideoFileClip
 
 from tools import ActivityDetectionTool, VideoActivityRecognitionTool, llm
+from langchain_core.prompts.chat import ChatPromptTemplate
 
 load_dotenv()
 
 YOLO_CLASSIFICATION_PATH = os.getenv('YOLO_CLASSIFICATION_PATH')
 YOLO_TRACKING_PATH = Path("runs/detect/custom_yolo3/weights/best.pt") # TODO put in .env
 
+chat_prompt_template = ChatPromptTemplate.from_messages(
+    [
+        ("system", "You are a helpful assistant."),
+        ("placeholder", "{messages}"),
+    ]
+)
+
+def modify_messages(messages: list):
+    return chat_prompt_template.invoke({"messages": messages})
+
 class App:
     def __init__(self, device) -> None:
-        self.llm = llm
-        memory = MemorySaver()
 
-        if "video_agent" not in st.session_state:
-            video_tools = [VideoActivityRecognitionTool().setup()]
-            st.session_state.video_agent = create_react_agent(llm, video_tools, checkpointer=memory)
+        if "chat" not in st.session_state:
+            st.session_state.chat = llm
 
-        if "act_classification_agent" not in st.session_state:
-            obj_tools = [ActivityDetectionTool().setup(YOLO(Path(YOLO_CLASSIFICATION_PATH).absolute(), verbose=False))]
-            st.session_state.act_classification_agent = create_react_agent(llm, obj_tools, checkpointer=memory)
+        if "agent" not in st.session_state:
+            memory = MemorySaver()
 
-        if "arm_tracking" not in st.session_state:
-            st.session_state.arm_tracking = VideoArmTrackingTool().setup(YOLO(Path(YOLO_TRACKING_PATH).absolute(), verbose=False))
+            video_tools = [
+                VideoActivityRecognitionTool().setup(), 
+                ActivityDetectionTool().setup(YOLO(Path(YOLO_CLASSIFICATION_PATH).absolute(), verbose=False))
+                ]
 
-    def process_video(self, clip_path):
+            st.session_state.agent = create_react_agent(llm, video_tools, checkpointer = memory, messages_modifier = modify_messages)
+
+        if "tracking" not in st.session_state:
+            st.session_state.tracking = VideoArmTrackingTool().setup(YOLO(Path(YOLO_TRACKING_PATH).absolute(), verbose=False))
+
+    def process_video_to_mkv(self, clip_path):
         clip = VideoFileClip(clip_path)
         clip.write_videofile(clip_path.with_suffix(".mkv"))
         return clip_path.with_suffix(".mkv")
-    
-    def _process_file(self, prompt, file_path: str) -> None:
-        try:
-            config = {"configurable": {"thread_id": "abc123"}}
-            
-            # Extract necessary objects from session state to pass to worker functions
-            video_agent = st.session_state.video_agent
-            obj_agent = st.session_state.act_classification_agent
-            arm_tracking = st.session_state.arm_tracking
-
-            with ThreadPoolExecutor() as executor:
-                # Submit the tasks to the executor
-                future_video_description = executor.submit(get_video_description, video_agent, prompt, file_path, config) # NO
-                future_object_response = executor.submit(get_object_response, obj_agent, prompt, file_path, config) # NO
-                future_tracked_video = executor.submit(track_video, arm_tracking, file_path) # YES
-
-                # Wait for all futures to complete and collect results
-                for future in as_completed([future_video_description, future_object_response, future_tracked_video]):
-                    if future == future_video_description:
-                        video_description = future.result()
-                    elif future == future_object_response:
-                        object_response = future.result()
-                    elif future == future_tracked_video:
-                        tracked_video_directory = future.result()
-
-            # After all tasks have completed, process the video to convert it
-            tracked_video_directory = self.process_video(tracked_video_directory)
-
-            # Log results
-            logger.debug(f"{video_description=}")
-            logger.debug(f"{object_response=}")
-            logger.debug(f"{tracked_video_directory=}")
-            return video_description["messages"][-1].content, object_response["messages"][-1].content, tracked_video_directory
-
-        except Exception as e:
-            logger.error(e)
-
 
     def _copy_file(self, uploaded_image):
         tmp_dir = "tmp/"
         os.makedirs(tmp_dir, exist_ok=True)
-        temp_file_path = os.path.join(tmp_dir, uploaded_image.name)
+        temp_file_path = Path(f"{tmp_dir}/{uploaded_image.name}")
         with open(temp_file_path, "wb") as file:
             file.write(uploaded_image.getvalue())
+        return temp_file_path
     
     def _upload_image(self) -> None:
         uploaded_image = st.file_uploader("Upload an image")
@@ -97,10 +75,7 @@ class App:
             self._process_image(temp_file_path)
 
     def run(self) -> None:
-        st.title("Chat with LLava")
-
-        if "chat" not in st.session_state:
-            st.session_state.chat = self.llm
+        st.title("Chat with RoboArmVision")
 
         if "messages" not in st.session_state:
             st.session_state.messages = []
@@ -116,64 +91,62 @@ class App:
         # display the chat messages
         for message in st.session_state.messages:
             with st.chat_message(message.type):
-                st.markdown(message.content.split(". Answer given")[0])
+                st.markdown(message.content)
                 if "file_path" not in message.additional_kwargs:
                     continue
 
-                # image
+                # image TODO
                 # if message.additional_kwargs["file_path"].suffix[1:] in ["jpg", "jpeg", "png"]:
                 #     st.image(str(message.additional_kwargs["file_path"]))
 
                 # video
-                if message.additional_kwargs["file_path"].suffix[1:] in ["mp4", "mkv"]:
+                if message.additional_kwargs["file_path"].suffix in [".mp4", ".mkv"]:
                     st.video(str(message.additional_kwargs["file_path"]))
 
         if submit_button:
             message = HumanMessage(content=prompt)
 
             if uploaded_file is not None:
-                self._copy_file(uploaded_file)
-                tmp_path = Path(f"tmp/{uploaded_file.name}")
+                tmp_path = self._copy_file(uploaded_file)
+                tracker = st.session_state.tracking
 
-                video_description, activity_detection, tracked_video_directory = self._process_file(message.content, tmp_path)
-                message.content += ". Answer given these informations about the file, explain with simple words, don't write the file path, maintain the video description as it is:\n"
-                if video_description:
-                    message.content += f"Description of the video in a healthcare environment carried out by two robotic arms: '{video_description}', maintain the description in the response'\n"
-                if activity_detection:
-                    message.content += f"Detected activities of the video with corresponding start and end timestamps: '{activity_detection}'\n"
-
-                message.additional_kwargs.update({"file_path": tracked_video_directory})
+                with ProcessPoolExecutor() as executor:
+                # Submit the tasks to the executor
+                    tracked_video_directory = executor.submit(tracker.track, tmp_path)
+                
+                message.additional_kwargs.update({"file_path": tmp_path})
 
             # Append user's message to session state
             st.session_state.messages.append(message)
-            with st.chat_message("user"):
+            with st.chat_message(message.type):
                 if prompt:
                     st.markdown(prompt)
                 if uploaded_file is not None:
                     if "image" in uploaded_file.type:
                         st.image(uploaded_file)
                     if "video" in uploaded_file.type:
-                        st.video(str(tracked_video_directory))
+                        st.video(str(tmp_path))
 
             # Get response from the LLM
             with st.chat_message("assistant"):
-                print(st.session_state.messages)
-                response = st.session_state.chat.invoke(st.session_state.messages)
-                response.pretty_print()
-                st.markdown(response.content)
+                config = {"configurable": {"thread_id": "1"}}
+                if uploaded_file:
+                    mess = st.session_state.messages.copy()
+                    mess[-1].content += f" {tmp_path}" # TODO inject path not via prompt
+                    response = st.session_state.agent.invoke({"messages": mess}, config)
+                    response = response["messages"][-1]
+                    st.markdown(response.content)
+                    video = self.process_video_to_mkv(tracked_video_directory.result())
+                    st.video(str(video))
+                    response.additional_kwargs["file_path"] = video
+                else:
+                    response = st.session_state.chat.invoke(st.session_state.messages)
+                    st.markdown(response.content)
+
+                # response.pretty_print()
+                # logger.debug(f"{response=}")
                 st.session_state.messages.append(response)
             print(f"{st.session_state.messages=}")
-
-def get_video_description(agent, prompt, file_path, config):
-    return agent.invoke(
-        {"messages": [HumanMessage(content=f"{prompt}. Given the following video:{file_path}. Answer with an exaustive video description")]}, config)
-
-def get_object_response(agent, prompt, file_path, config):
-    return agent.invoke(
-        {"messages": [HumanMessage(content=f"{prompt}. Given the following video:{file_path}. Write it in a more readable way for the common user.")]}, config)
-
-def track_video(agent, file_path):
-    return agent.track(file_path)
 
 if __name__ == "__main__":
     device = "cuda" if torch.cuda.is_available() else "cpu"
